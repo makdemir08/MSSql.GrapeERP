7314426211ed540edd5b828665cf27d83067eb73
Mustafa Akdemir <makdemir08@hotmail.com>
2022-05-08 11:42:30 +0300

    nvarchar field type to varchar convert

--- CreateTable_Log.ErrorLog.sql
+++ CreateTable_Log.ErrorLog.sql
@@ -1,14 +1,14 @@
 CREATE TABLE [Log].[ErrorLog](
 	[ErrorLogId] [bigint] IDENTITY(1,1) NOT NULL,
 	[ErrorDate] [datetime] NULL,
-	[ProcedureName] [nvarchar](50) NULL,
-	[ErrorMessage] [nvarchar](250) NULL,
+	[ProcedureName] [varchar](50) NULL,
+	[ErrorMessage] [varchar](250) NULL,
 	[ErrorLine] [int] NULL,
 	[ErrorNumber] [int] NULL,
 	[ErrorSeverity] [int] NULL,
 	[ErrorState] [int] NULL,
-	[UserName] [nvarchar](25) NULL,
-	[HostName] [nvarchar](25) NULL,
+	[UserName] [varchar](25) NULL,
+	[HostName] [varchar](25) NULL,
  CONSTRAINT [PK_ErrorLog] PRIMARY KEY CLUSTERED 
 (
 	[ErrorLogId] ASC
